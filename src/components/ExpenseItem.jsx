93f2eeef0be8f68458df9314ef77fb436a0d801f
Al Bur <albur2021@yahoo.com>
2021-08-11 14:22:55 -0400

    passing Date through props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,14 @@
 import './ExpenseItem.css';
-const expenseDate = new Date(2021, 7, 28);//initialize date 
-const expenseTitle = "Car insurance";
-const expenseAmout = 294.07;
+
 const ExpenseItem = (props) => {
   return (
     // We can't have two HTML root element here, hence we wrap all our tags
     <div className="expense-item">
-      <div >{expenseDate.toISOString()}</div>
+      <div >{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">{expenseAmout}</div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
 };
