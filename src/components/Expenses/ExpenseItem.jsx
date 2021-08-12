997490bb512a6af8647f880875505a6759a7e54b
Al Bur <albur2021@yahoo.com>
2021-08-11 20:17:24 -0400

    add useState and create button in ExpenseItem branch

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,16 +2,22 @@ import "./ExpenseItem.css";
 import "./ExpenseDate";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from '../UI/Card';
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
+  useState(props.title);
+  const [title, setTitle] = useState(props.title);
+  
+
   const clickHandler = () =>{
-    console.log('Clicked!');
+    setTitle("Updated!");
+    console.log(title);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
       <button onClick={clickHandler}>Change Title</button>
