926f942b34f6e7ca90f633f493f3caee0a7fed4a
Al Bur <albur2021@yahoo.com>
2021-08-11 22:16:36 -0400

    Add Two-Way Binding to SubmitHandler Method and add value into HTML/JSX Return

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -54,12 +54,20 @@ const ExpenseForm = (props) => {
      const submitHandler = (event) =>{
         event.preventDefault();//prevent from refreshing webpage by browser
         
+        //Storing date into this object:
+        //Two-way binding state: store and change/clean data from UI
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
         console.log(expenseData);
+
+        //Updating/Empty data after data were stored
+        
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEntereDate('');
      };
 
   return (
@@ -67,15 +75,31 @@ const ExpenseForm = (props) => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input 
+          type="text" 
+          value={enteredTitle} 
+          onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input 
+          type="number" 
+          min="0.01" 
+          step="0.01" 
+          onChange={amountChangeHandler}
+          value={enteredAmount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+          <input 
+          type="date" 
+          min="2019-01-01" 
+          max="2022-12-31" 
+          onChange={dateChangeHandler}
+          value={enteredDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
